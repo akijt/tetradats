20af127a520d4fd94496ffdf17e242072ee997f2
akijt <123210724+akijt@users.noreply.github.com>
2023-08-18 14:11:36 -0400

    Sprite update
    Implemented Sprite_text class in utils.py
    Streamlined state_menu.py

--- state_menu.py
+++ state_menu.py
@@ -1,34 +1,43 @@
 import pygame
-from utils import Sprite_rect
+from utils import Sprite_rect, Sprite_text
 
 def state_menu(screen, clock, state, user_info):
 
     ### INIT STATE
-    marathon_button = Sprite_rect((14, 2), 'midbottom', (0, -4), 'center', 'White')
-    sprint_button = Sprite_rect((14, 2), 'midbottom', (0, -1), 'center', 'White')
-    blitz_button = Sprite_rect((14, 2), 'midbottom', (0, 2), 'center', 'White')
-    records_button = Sprite_rect((14, 2), 'midbottom', (0, 5), 'center', 'White')
-    settings_button = Sprite_rect((14, 2), 'midbottom', (0, 8), 'center', 'White')
-    logout_button = Sprite_rect((8, 2), 'bottomleft', (1, -1), 'bottomleft', 'White')
-    group = pygame.sprite.Group()
-    group.add(marathon_button)
-    group.add(sprint_button)
-    group.add(blitz_button)
-    group.add(records_button)
-    group.add(settings_button)
-    group.add(logout_button)
+    marathon_button = Sprite_rect((14, 2), 'midbottom', (0, -4), 'center', 'White', 2)
+    sprint_button   = Sprite_rect((14, 2), 'midbottom', (0, -1), 'center', 'White', 2)
+    blitz_button    = Sprite_rect((14, 2), 'midbottom', (0, 2), 'center', 'White', 2)
+    records_button  = Sprite_rect((14, 2), 'midbottom', (0, 5), 'center', 'White', 2)
+    settings_button = Sprite_rect((14, 2), 'midbottom', (0, 8), 'center', 'White', 2)
+    logout_button   = Sprite_rect((8, 2), 'bottomleft', (1, -1), 'bottomleft', 'White', 2)
+
+    menu_group = pygame.sprite.Group()
+    menu_group.add(marathon_button)
+    menu_group.add(sprint_button)
+    menu_group.add(blitz_button)
+    menu_group.add(records_button)
+    menu_group.add(settings_button)
+    menu_group.add(logout_button)
+
+    menu_group.add(Sprite_text('TETRADATS', 'midbottom', (0, -10), 'center', 'White', 4, None))
+    menu_group.add(Sprite_text('marathon', 'midbottom', (0, -4), 'center', 'White', 4, None))
+    menu_group.add(Sprite_text('sprint', 'midbottom', (0, -1), 'center', 'White', 4, None))
+    menu_group.add(Sprite_text('blitz', 'midbottom', (0, 2), 'center', 'White', 4, None))
+    menu_group.add(Sprite_text('records', 'midbottom', (0, 5), 'center', 'White', 4, None))
+    menu_group.add(Sprite_text('settings', 'midbottom', (0, 8), 'center', 'White', 4, None))
+    menu_group.add(Sprite_text('logout', 'midbottom', (5, -1), 'bottomleft', 'White', 4, None))
+
+    account_group = pygame.sprite.Group()
+    account_group.add(Sprite_rect((8, 2), 'topright', (-1, 1), 'topright', 'White', 2))
+    account_group.add(Sprite_rect((1.5, 1.5), 'topleft', (-8.75, 1.25), 'topright', 'White', 1))
+    account_group.add(Sprite_text(user_info['username'], 'bottomright', (-1.5, 2.6), 'topright', 'White', 2, None))
+
 
     while True:
 
-        ### ADJUST DIM
-        dim = min(screen.get_width() / 40, screen.get_height() / 30) # To fit in a 4:3 aspect ratio
-        fonts = []
-        fonts.append(pygame.font.Font(None, round(.75 * 2 * dim)))
-        fonts.append(pygame.font.Font(None, round(.75 * 4 * dim)))
-        border_width = 1
-
         ### UPDATE SPRITES
-        group.update(screen.get_size(), dim, border_width + 1)
+        menu_group.update(screen)
+        account_group.update(screen)
 
         ### EVENT LOOP
         for event in pygame.event.get():
@@ -67,50 +76,9 @@ def state_menu(screen, clock, state, user_info):
         ### CLEAR SCREEN
         pygame.draw.rect(screen, 'Black', screen.get_rect())
 
-        ### DRAW BUTTONS
-        group.draw(screen)
-
-        ### WRITE TEXT
-        state_text = fonts[1].render('TETRADATS', False, 'White')
-        state_rect = state_text.get_rect(midbottom=(screen.get_width() / 2, screen.get_height() / 2 - 10 * dim))
-        screen.blit(state_text, state_rect)
-
-        marathon_text = fonts[1].render('marathon', False, 'White')
-        marathon_rect = marathon_text.get_rect(midbottom=marathon_button.rect.midbottom)
-        screen.blit(marathon_text, marathon_rect)
-
-        sprint_text = fonts[1].render('sprint', False, 'White')
-        sprint_rect = sprint_text.get_rect(midbottom=sprint_button.rect.midbottom)
-        screen.blit(sprint_text, sprint_rect)
-
-        blitz_text = fonts[1].render('blitz', False, 'White')
-        blitz_rect = blitz_text.get_rect(midbottom=blitz_button.rect.midbottom)
-        screen.blit(blitz_text, blitz_rect)
-
-        records_text = fonts[1].render('records', False, 'White')
-        records_rect = records_text.get_rect(midbottom=records_button.rect.midbottom)
-        screen.blit(records_text, records_rect)
-
-        settings_text = fonts[1].render('settings', False, 'White')
-        settings_rect = settings_text.get_rect(midbottom=settings_button.rect.midbottom)
-        screen.blit(settings_text, settings_rect)
-
-        logout_text = fonts[1].render('logout', False, 'White')
-        logout_rect = logout_text.get_rect(midbottom=logout_button.rect.midbottom)
-        screen.blit(logout_text, logout_rect)
-
-        ### ACCOUNT TAB
-        account_tab = pygame.Rect(0, 0, 8 * dim, 2 * dim)
-        account_tab.topright = (screen.get_width() - 1 * dim, 1 * dim)
-        pygame.draw.rect(screen, 'White', account_tab, border_width + 1)
-
-        account_text = fonts[0].render(user_info['username'], False, 'White')
-        account_rect = account_text.get_rect(bottomright=(account_tab.right - .5 * dim, account_tab.bottom - .4 * dim))
-        screen.blit(account_text, account_rect)
-
-        pfp_tab = pygame.Rect(0, 0, 1.5 * dim, 1.5 * dim)
-        pfp_tab.bottomleft = (account_tab.left + .25 * dim, account_tab.bottom - .25 * dim)
-        pygame.draw.rect(screen, 'White', pfp_tab, border_width)
+        ### DRAW SPRITES
+        menu_group.draw(screen)
+        account_group.draw(screen)
 
         ### CLOCK
         pygame.display.update()
--- utils.py
+++ utils.py
@@ -2,13 +2,14 @@ import pygame
 
 class Sprite_rect(pygame.sprite.Sprite):
 
-    def __init__(self, size, origin, offset, alignment, color):
+    def __init__(self, size, origin, offset, alignment, color, width):
         super().__init__()
         self.size      = size
         self.origin    = origin
         self.offset    = [x for x in offset]
         self.alignment = alignment
         self.color     = color
+        self.width     = width
         
         if self.origin in ['topright', 'midright', 'bottomright']:
             self.offset[0] -= self.size[0]
@@ -19,23 +20,60 @@ class Sprite_rect(pygame.sprite.Sprite):
         elif self.origin in ['midleft', 'center', 'midright']:
             self.offset[1] -= self.size[1] / 2
     
-    def update(self, screen_size, dim, width):
+    def update(self, screen):
+        dim = min(screen.get_width() / 40, screen.get_height() / 30) # To fit in a 4:3 aspect ratio
+
         self.image = pygame.Surface((self.size[0] * dim, self.size[1] * dim))
         self.rect = pygame.Rect(0, 0, self.size[0] * dim, self.size[1] * dim)
-        pygame.draw.rect(self.image, self.color, self.rect, width)
+        pygame.draw.rect(self.image, self.color, self.rect, self.width)
 
         self.rect.topleft = (self.offset[0] * dim, self.offset[1] * dim)
         if self.alignment in ['topright', 'midright', 'bottomright']:
-            self.rect.left += screen_size[0]
+            self.rect.left += screen.get_width()
         elif self.alignment in ['midtop', 'center', 'midbottom']:
-            self.rect.left += screen_size[0] / 2
+            self.rect.left += screen.get_width() / 2
         if self.alignment in ['bottomleft', 'midbottom', 'bottomright']:
-            self.rect.top += screen_size[1]
+            self.rect.top += screen.get_height()
         elif self.alignment in ['midleft', 'center', 'midright']:
-            self.rect.top += screen_size[1] / 2
+            self.rect.top += screen.get_height() / 2
 
 class Sprite_text(pygame.sprite.Sprite):
-    pass
+
+    def __init__(self, text, origin, offset, alignment, color, font_size, font_name=None):
+        super().__init__()
+        self.text      = text
+        self.origin    = origin
+        self.offset    = [x for x in offset]
+        self.alignment = alignment
+        self.color     = color
+        self.font_size = font_size
+        self.font_name = font_name
+        
+    def update(self, screen):
+        dim = min(screen.get_width() / 40, screen.get_height() / 30) # To fit in a 4:3 aspect ratio
+
+        font = pygame.font.Font(self.font_name, round(.75 * self.font_size * dim))
+        self.image = font.render(self.text, False, self.color)
+        self.rect = self.image.get_rect()
+
+        self.rect.topleft = (self.offset[0] * dim, self.offset[1] * dim)
+        if self.origin in ['topright', 'midright', 'bottomright']:
+            self.rect.left -= self.rect.width
+        elif self.origin in ['midtop', 'center', 'midbottom']:
+            self.rect.left -= self.rect.width / 2
+        if self.origin in ['bottomleft', 'midbottom', 'bottomright']:
+            self.rect.top -= self.rect.height
+        elif self.origin in ['midleft', 'center', 'midright']:
+            self.rect.top -= self.rect.height / 2
+
+        if self.alignment in ['topright', 'midright', 'bottomright']:
+            self.rect.left += screen.get_width()
+        elif self.alignment in ['midtop', 'center', 'midbottom']:
+            self.rect.left += screen.get_width() / 2
+        if self.alignment in ['bottomleft', 'midbottom', 'bottomright']:
+            self.rect.top += screen.get_height()
+        elif self.alignment in ['midleft', 'center', 'midright']:
+            self.rect.top += screen.get_height() / 2
 
 class Sprite_button(pygame.sprite.Sprite):
     pass
